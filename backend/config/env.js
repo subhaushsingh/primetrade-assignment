d44788d0bf48a138209669f83c540a6e3d80d2d7
Subh Aush Singh <subhaushsingh@gmail.com>
2026-05-01 23:13:23 +0530

    added frontend url

--- backend/config/env.js
+++ backend/config/env.js
@@ -6,5 +6,6 @@ export const env = {
   PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV || "development",
   MONGO_URI: process.env.MONGO_URI,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  FRONTEND_URL: process.env.FRONTEND_URL
 };
