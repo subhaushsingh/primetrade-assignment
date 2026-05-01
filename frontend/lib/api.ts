7fb8829c3044e61f1d6e0f5b4fed50588ab1ca33
Subh Aush Singh <subhaushsingh@gmail.com>
2026-05-01 23:39:39 +0530

    Baseurl bug fixed

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? '/_/backend/api' 
+  ? '/_/backend/api/v1' 
   : 'http://localhost:5000/api';
 
 const api = axios.create({
